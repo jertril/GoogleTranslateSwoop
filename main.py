1192ae28f38afb522e50ce0c3f500dc52cb4da79
c0nn3r <connervercellino@gmail.com>
2017-01-11 10:15:31 -0800

    Added ROS items.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import re
 import json
+import rospy
 import requests
+import actionlib
+
+from pygame import mixer
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
@@ -15,16 +20,34 @@ def get_google_translate_token(text):
         raise "Token server is down."
 
 
+def convert(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+
+
 def say(text):
 
     response = requests.get('https://translate.google.com/translate_tts?ie=UTF-8&q=' + text + '&tl=en&total=' + str(len(text)) + '&idx=0&textlen=5&tk=' + get_google_translate_token(text) + '&client=t&prev=input', headers=headers)
 
     print('Downloading %s...' % (text))
 
-    with open(text+'.mp3', 'wb') as fo:
+    with open(covert(text) +'.mp3', 'wb') as fo:
 
         for chunk in response.iter_content(4096):
             fo.write(chunk)
 
-say("hello")
+
+    mixer.init()
+    mixer.music.load(covert(text) + '.mp3')
+    mixer.music.play()
+
+def text_to_speech(TextClass):
+    say(TextClass.text)
+    server.set_succeeded('I did it, look at me!')
+
+rospy.init_node('voice')
+server = actionlib.SimpleActionServer('text_to_speech', Text, say, False)
+
+server.start()
+rospy.spin()
+
 
